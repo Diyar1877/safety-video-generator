035f7c038f51c7a6068048c9cdbea3d3baa1425c
diyar <diyar.mohammed@safety-doors.com>
2026-03-15 23:35:31 +0100

    contact vormular verbessert

--- src/app/app.ts
+++ src/app/app.ts
@@ -47,6 +47,7 @@ export class App {
     const dialogRef = this.dialog.open(ContactDialogComponent, {
       width: '680px',
       maxWidth: '95vw',
+      maxHeight: '95vh',
       panelClass: 'contact-dialog-panel',
       autoFocus: false,
     });
